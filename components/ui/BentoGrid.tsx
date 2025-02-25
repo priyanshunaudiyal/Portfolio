8b491c83da9787e8d650de879f00742a3771339f
priyanshunaudiyal <priyanshubhardwaj51@gmail.com>
2025-02-25 17:57:18 +0530

    Button changed

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -47,11 +47,7 @@ export const BentoGridItem = ({
         className
       )}
 
-      style={{
-        background: 'rgb(4,7.29',
-        backgroundColor:
-          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-      }}
+      
     >
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
@@ -64,3 +60,10 @@ export const BentoGridItem = ({
     </div>
   );
 };
+
+
+// style={{
+//   background: "rgb(4,7,29)",
+//   backgroundColor:
+//     "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)"
+// }}
--- components/ui/SimpleButton.tsx
+++ components/ui/SimpleButton.tsx
@@ -1,25 +1,21 @@
 import React from 'react'
 
 const SimpleButton = ({
-    title, 
-    icon, 
-    position, 
-    handleClick, 
+    title,
+    icon,
+    position,
+    handleClick,
     otherClasses = ""
 }: {
-    title: string; 
-    icon: React.ReactNode; 
-    position: string; 
-    handleClick?: () => void; 
+    title: string;
+    icon: React.ReactNode;
+    position: string;
+    handleClick?: () => void;
     otherClasses?: string
 }) => {
     return (
-        <button 
-            onClick={handleClick}
-            className={`px-5 py-3 rounded-md border border-black dark:border-white bg-white dark:bg-black text-black dark:text-white text-sm font-bold hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] dark:hover:shadow-[4px_4px_0px_0px_rgba(255,255,255,1)] transition duration-200 flex items-center ${position === 'left' ? 'flex-row' : 'flex-row-reverse'} gap-2 ${otherClasses}`}
-        >
-            {icon && <span className={position === 'left' ? 'mr-1' : 'ml-1'}>{icon}</span>}
-            <span>{title}</span>
+        <button className="px-4 py-2 rounded-md border border-neutral-300 bg-neutral-100 text-neutral-500 text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
+            {title}
         </button>
     )
 }
